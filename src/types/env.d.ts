4fa858fd8fb550e19c96d1fa16fa09a099cf4038
unknown <brhimakil.1@gmail.com>
2025-01-19 00:30:28 -0800

    18th commit

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,8 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_SUPABASE_URL: string;
-      NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
       NEXT_PUBLIC_GEMINI_API_KEY?: string;
     }
   }
